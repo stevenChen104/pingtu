a77138779f6a42698166b1be8c0720a9d5e9c188
start810813 <start810813@gmail.com>
2022-11-10 12:01:33 +0800

    refactor&fix: 物件化  & 修正樣式錯誤

--- src/assets/javascripts/common.js
+++ src/assets/javascripts/common.js
@@ -1,188 +1,244 @@
-var file_seq = 0;
-var promises = [];
+// helper
 
-window.addEventListener('load', function () {
-    document.querySelector('#uploadFile').addEventListener('change', function () {
-        uploadNewFile(this);
-    });
-});
-
-function uploadNewFile(input) {
-    if (input.files.length > 0) {
-        var sort_type = document.querySelector('input[type=radio][name="sort"]:checked').value;
-        switch (sort_type) {
-            case 'byUpload':
-                var files = input.files;
-                break;
-            case 'byName':
-                var files = sortFilesByName(input.files);
-                break;
-            case 'byUser':
-                break;
-        }
-        setupReader(files, 0);
-    }
+const render = (node, appendElement, position = 'beforeend') => {
+  if (!node || !appendElement) return
+  if (position) {
+    node.insertAdjacentElement(position, appendElement)
+  }
 }
 
-function sortFilesByName(files) {
-    for (var i = 0; i < files.length; i++) {
-        files[i].seq = Number(files[i].name.replace(/\D/g, ''));
-    }
-    files = _.orderBy(files, ['seq'], ['asc']);
-    return files;
+const showWhile = (modalName) => {
+  const modal = document.querySelector('#' + modalName)
+  fadeIn(modal)
+  window.setTimeout(() => {
+    fadeOut(modal)
+  }, 1500)
 }
 
-function setupReader(files, pos) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        readAndLoadFile(e, files, pos);
-    };
-    reader.readAsDataURL(files[pos]);
-}
-
-function readAndLoadFile(e, files, pos) {
-    var file = files[pos];
-    var file_name = file.name;
-    var file_base64 = e.target.result;
-    var slider = document.querySelector('.preview-area');
-    var node = document.querySelector('.preview-area .upload-item');
-    var template = '\
-        <div class="image-container" οndrοp="drop(event,this)" οndragοver="allowDrop(event)" draggable="true" οndragstart="drag(event, this)">\
-            <div class="preview-image" style="background-image: url(\'' + file_base64 + '\'");">\
-                <div class="remove-icon" onclick="removeImageContainer(this)"></div>\
-            </div>\
-            <img class="hide real-image" src="' + file_base64 + '"></img>\
-            <div class="file-seq"></div>\
-            <div class="file-name">' + file_name + '</div>\
-        </div>';
-    render(node, template, 'beforebegin');
-    addSeqToPreviewer();
-    slider.scrollLeft = slider.scrollWidth;
-    if (pos < files.length - 1) {
-        setupReader(files, pos + 1);
-    }
-}
-
-function addSeqToPreviewer() {
-    var _containers = document.querySelectorAll('.preview-area .image-container');
-    for (var i = 0; i < _containers.length; i++) {
-        _containers[i].querySelector('.file-seq').innerText = i + 1;
-    }
-}
-
-function triggerUpload() {
-    document.querySelector('.input-area input[type=file]').click();
-}
-
-function render(node, template, position) {
-    if (!node) return;
-    if (position) {
-        node.insertAdjacentHTML(position, template);
+const fadeIn = (el) => {
+  el.classList.remove('hide')
+  el.style.opacity = 0
+  let last = +new Date()
+  const tick = () => {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 100
+    last = +new Date()
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 1)
     } else {
-        node.insertAdjacentHTML('beforeend', template);
+      el.style.opacity = 1
     }
+  }
+  tick()
 }
 
-function removeNode(node) {
-    node.parentNode.removeChild(node);
-}
-
-function pingtu() {
-    var canvas = document.querySelector('#resultImage');
-    if (canvas.getContext) {
-        var result_area = document.querySelector('.result-area');
-        var images = document.querySelectorAll('.real-image');
-        var ctx = canvas.getContext('2d');
-        var total_height = 0;
-        var total_width = 0;
-        if (images.length > 0) {
-            // 調整畫布大小
-            _.forEach(images, function (img) {
-                var this_height = img.height;
-                var this_width = img.width;
-                total_width = this_width > total_width ? this_width : total_width;
-                total_height = total_height + this_height;
-            });
-            console.log(total_width);
-            console.log(total_height)
-            canvas.height = total_height;
-            canvas.width = total_width;
-            // 拼接圖片
-            total_height = 0;
-            total_width = 0;
-            _.forEach(images, function (img) {
-                var this_height = img.height;
-                var this_width = img.width;
-                ctx.drawImage(img, total_width, total_height, this_width, this_height);
-                total_height = total_height + this_height;
-            });
-            result_area.classList.remove('hide');
-            showWhile('welldone');
-        } else {
-            showWhile('nothing');
-        }
-
+const fadeOut = (el) => {
+  el.style.opacity = 1
+  let last = +new Date()
+  const tick = () => {
+    el.style.opacity = +el.style.opacity - (new Date() - last) / 100
+    last = +new Date()
+    if (+el.style.opacity > 0) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 1)
     } else {
-        alert('網站目前不支援您的瀏覽器');
+      el.style.opacity = 0
+      el.classList.add('hide')
     }
+  }
+  tick()
 }
 
-function removeImageContainer(ele) {
-    var _container = ele.parentNode.parentNode;
-    removeNode(_container);
-    addSeqToPreviewer();
+const triggerUpload = () => {
+  document.querySelector('.input-area input[type=file]').click()
 }
 
-function clearImages() {
-    var _remove_buttons = document.querySelectorAll('.preview-area .image-container .remove-icon');
-    var result_area = document.querySelector('.result-area');
-    _.forEach(_remove_buttons, function (button) {
-        button.click();
-    });
-    result_area.classList.add('hide');
+const getSortType = () => {
+  return document.querySelector('input[type=radio][name="sort"]:checked').value
 }
 
-function showWhile(modalName) {
-    var modal = document.querySelector('#' + modalName);
-    fadeIn(modal);
-    window.setTimeout(function () {
-        fadeOut(modal);
-    }, 1500);
+const hideResultArea = () => {
+  document.querySelector('.result-area').classList.add('hide')
 }
 
-function fadeIn(el) {
-    el.classList.remove('hide');
-    el.style.opacity = 0;
-    var last = +new Date();
-    var tick = function () {
-        el.style.opacity = +el.style.opacity + (new Date() - last) / 100;
-        last = +new Date();
-        if (+el.style.opacity < 1) {
-            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 1);
-        } else {
-            el.style.opacity = 1;
-        }
-    };
-    tick();
+const clearPreviewElement = () => {
+  const imageContainers = document.querySelectorAll('.image-container')
+
+  imageContainers.forEach((container) => {
+    container.remove()
+  })
 }
 
-function fadeOut(el) {
-    el.style.opacity = 1;
-    var last = +new Date();
-    var tick = function () {
-        el.style.opacity = +el.style.opacity - (new Date() - last) / 100;
-        last = +new Date();
-        if (+el.style.opacity > 0) {
-            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 1);
-        } else {
-            el.style.opacity = 0;
-            el.classList.add('hide');
-        }
-    };
-    tick();
+const getBase64 = (file) => {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      resolve(e.target.result)
+    }
+    reader.readAsDataURL(file)
+  })
 }
 
-function changeSortType(sort_type) {
-    var _sort_type = document.querySelector('input[type=radio][name="sort"][value="' + sort_type + '"]');
-    _sort_type.checked = true;
+const uploadNewFile = async (input, previewer) => {
+  if (input.files.length > 0) {
+    await previewer.appendImages([...input.files])
+    previewer.preview()
+  }
+}
+
+const sortImageByName = (images) => {
+  return images.sort((a, b) => Number(a.file.name.replace(/\D/g, '')) - Number(b.file.name.replace(/\D/g, '')))
+}
+
+// Objects
+class Image {
+  constructor (file, seq) {
+    this.file = file
+    this.seq = seq
+    this.base64 = null
+  }
+  async setBase64 () {
+    this.base64 = await getBase64(this.file)
+  }
+}
+
+class Previewer {
+  constructor () {
+    this.slider = document.querySelector('.preview-area')
+    this.uploadItem = document.querySelector('.preview-area .upload-item')
+    this.images = []
+  }
+
+  async appendImages (files) {
+    const sortType = getSortType()
+    const images = []
+    const seqOffset = this.images.length + 1
+    for (const index in files) {
+      const file = files[index]
+      const image = new Image(file, seqOffset + Number(index))
+      await image.setBase64()
+      images.push(image)
+    }
+    const outputImages = [...this.images, ...images]
+    this.images = outputImages
+
+    if (sortType === 'byName') {
+      const sortedImages = sortImageByName(outputImages)
+      sortedImages.forEach((image, index) => image.seq = index + 1)
+      this.images = sortedImages
+    }
+  }
+
+  preview () {
+    clearPreviewElement()
+    this.images.forEach((image) => {
+      this.create(image)
+    })
+  }
+  
+  create (image) {
+    const fileName = image.file.name
+    const seq = image.seq
+    const base64 = image.base64
+    const containerDiv = document.createElement('div')
+    const previewImageDiv = document.createElement('div')
+    const removeIconDiv = document.createElement('div')
+    const realImageImg = document.createElement('img')
+    const fileSeqDiv = document.createElement('div')
+    const fileNameDiv = document.createElement('div')
+
+    removeIconDiv.classList.add('remove-icon')
+    removeIconDiv.dataset.seq = seq
+    removeIconDiv.onclick = () => this.remove(seq)
+    previewImageDiv.classList.add('preview-image')
+    previewImageDiv.style.backgroundImage = `url('${base64}')`
+    previewImageDiv.appendChild(removeIconDiv)
+    realImageImg.classList.add('real-image', 'hide')
+    realImageImg.src = base64
+    fileSeqDiv.classList.add('file-seq')
+    fileSeqDiv.innerText = seq
+    fileNameDiv.classList.add('file-name')
+    fileNameDiv.innerText = fileName
+    containerDiv.classList.add('image-container')
+    containerDiv.appendChild(previewImageDiv)
+    containerDiv.appendChild(realImageImg)
+    containerDiv.appendChild(fileSeqDiv)
+    containerDiv.appendChild(fileNameDiv)
+
+    render(this.uploadItem, containerDiv, 'beforebegin')
+    this.slider.scrollLeft = this.slider.scrollWidth
+  }
+
+  remove (seq) {
+    this.images = this.images.filter((image) => image.seq !== seq)
+    this.preview()
+  }
+
+  clear () {
+    this.images = []
+    clearPreviewElement()
+    hideResultArea()
+  }
+}
+
+class Canvas {
+  constructor (images) {
+    this.images = images
+  }
+
+  setImages (images) {
+    this.images = images
+  }
+}
+
+
+// main program
+window.addEventListener('load', () => {
+  const previewer = new Previewer()
+  document.querySelector('#addFileButton').addEventListener('click', triggerUpload)
+  document.querySelector('#clearButton').addEventListener('click', previewer.clear.bind(previewer))
+  document.querySelector('#submitButton').addEventListener('click', pingtu)
+  document.querySelector('#uploadFile').addEventListener('change', function () {
+    uploadNewFile(this, previewer)
+  })
+})
+
+//
+const pingtu = () => {
+  const canvas = document.querySelector('#resultImage')
+  if (canvas.getContext) {
+    const resultArea = document.querySelector('.result-area')
+    const images = document.querySelectorAll('.real-image')
+    const ctx = canvas.getContext('2d')
+    let totalHeight = 0
+    let totalWidth = 0
+    let thisHeight = 0
+    let thisWidth = 0
+    if (images.length > 0) {
+      // 調整畫布大小
+      // getCanvasSize
+      images.forEach((img) => {
+        thisHeight = img.height
+        thisWidth = img.width
+        totalWidth = thisWidth > totalWidth ? thisWidth : totalWidth
+        totalHeight = totalHeight + thisHeight
+      })
+      canvas.height = totalHeight
+      canvas.width = totalWidth
+      // 拼接圖片
+      // draw
+      totalHeight = 0
+      totalWidth = 0
+      images.forEach((img) => {
+        thisHeight = img.height
+        thisWidth = img.width
+        ctx.drawImage(img, totalWidth, totalHeight, thisWidth, thisHeight)
+        totalHeight = totalHeight + thisHeight
+      })
+      resultArea.classList.remove('hide')
+      showWhile('welldone')
+    } else {
+      showWhile('nothing')
+    }
+  } else {
+    alert('網站目前不支援您的瀏覽器')
+  }
 }
